3329048966a5e6954d09de6381385158ca2a7cd9
Daniel de Souza Lima <39025412+DeWaldman@users.noreply.github.com>
2019-08-23 21:11:38 -0300

    deixando o código bonito

--- faztudo2001.py
+++ faztudo2001.py
@@ -4,66 +4,50 @@ from ast import literal_eval
 
 client = discord.Client()
 
+#-------------------------------------------------------------------------------------------
 
-async def RegistraMagia(message):
-    #le o arquivo(txt) e 'converte' em dicionario
-    with open ('testetxt') as le:
-        variavel=literal_eval(le.read())
+class RPG():
+  async def RegistraMagia(message):
+    with open ('grimorio') as grimorio: variavel=literal_eval(grimorio.read())
     msgr=message.content.lower().replace("!r","").split()
-    #adiciona valor na variavel correspondente ao txt
-    #msgr[0]= nome da magia;
-    #msgr[-2]= dado da magia;
-    #msgr[-1]= quantidade de dados
+    #msgr[0]= nome da magia; #msgr[-2]= dado da magia; #msgr[-1]= quantidade de dados
     variavel[" ".join(msgr[0:-2])]=f'{msgr[-2]} {msgr[-1]}'
-    #escreve o valor da variavel no txt
-    with open ('testetxt', 'w') as escreve:
-        escreve.write(str(variavel))
+    with open ('grimorio', 'w') as grimorio: grimorio.write(str(variavel))
     await message.channel.send("Sua magia foi registrada no grimório com sucesso")
 
-async def CastMagia(message):
-     #le o arquivo(txt) e 'converte' em dicionario
-    with open ('testetxt') as le:
-        variavel=literal_eval(le.read())
 
-    #recebe os valores da chave em '[]'
+  async def CastMagia(message):
+    with open ('grimorio') as le: variavel=literal_eval(le.read())
     valores=variavel[message.content.lower().replace("!cast","").strip()]
-
     valores=valores.split()
     await RollDice(message,valores,True)
-    
-async def CharlieBrown(message):
-    await message.channel.send('brown'+message.content.lower().replace("charlie","").replace("!",""))
 
-async def RollDice(message=None, valores=None, flag=False):
-    #lista[0]=tipo de dado
-    #lista[1]=vezes a rolar
+  async def RollDice(message=None, valores=None, flag=False):
+    #lista[0]=tipo de dado #lista[1]=vezes a rolar
     resul=[]
-    if flag:
-        lista=valores
-    else:
-        lista=message.content.lower().replace("!d","").split()
+    if flag: lista=valores
+    else: lista=message.content.lower().replace("!d","").split()
     for x in range(int(lista[1])):
-        resul.append(randint(1,int(lista[0].replace("d",""))))
+      resul.append(randint(1,int(lista[0].replace("d",""))))
     await message.channel.send(str(resul)[1:-1])
+
+async def CharlieBrown(message):
+  await message.channel.send('brown'+message.content.lower().replace("charlie","").replace("!",""))
         
-    
+
+#---------------------------------------------------------------------------------------------
+
+
 @client.event
 async def on_ready():
-     print(f'Logante como {client.user}')
+  print(f'Logante como {client.user}')
 
 @client.event
 async def on_message(message):
-    if message.author == client.user: return None
-        
-    if message.content.lower().startswith('!'):
-        if message.content.lower().replace("!","").startswith('charlie'):
-            await CharlieBrown(message)
-        if message.content.lower().replace("!","").startswith('d'):
-            await RollDice(message)
-        if message.content.lower().replace("!","").startswith('r'):
-            await RegistraMagia(message)
-        if message.content.lower().replace("!","").startswith('cast'):
-            await CastMagia(message)
-        
+  if message.author == client.user: return None
+  
+  comandos = {'!r':RPG.RegistraMagia, '!d':RPG.RollDice, '!charlie':CharlieBrown, '!cast':RPG.CastMagia}
+  if message.content.lower().startswith(tuple(comandos.keys())):
+    await comandos[message.content.lower().split()[0]](message)
 
-client.run('your token here')
+client.run('Token?')
